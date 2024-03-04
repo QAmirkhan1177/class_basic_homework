36f2a4d331880294e4516e113ff24943f01b4fcb
QAmirkhan1177 <amirxonqarshiyev@gmail.com>
2024-03-04 17:41:17 +0500

    3 tayyor

--- create_some_objects_to_list.py
+++ create_some_objects_to_list.py
@@ -5,3 +5,9 @@ from create_one_attribute import Person
 #Create an object named "p3" whose name is "Jasur"
 
 #Add these objects to the "persons" named list
+class Person:
+    def __init__(self,p1,p2,p3) -> None:
+        self.p1=p1
+        self.p2=p2
+        self.p3=p3
+Ali=Person()
