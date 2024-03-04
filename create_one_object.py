621bcc38635f471225793365be99a32fd293a094
QAmirkhan1177 <amirxonqarshiyev@gmail.com>
2024-03-04 17:37:47 +0500

    2 tayyor

--- create_one_object.py
+++ create_one_object.py
@@ -1,3 +1,7 @@
 from create_one_attribute import Person
 
 #create an object named "person" whose name is "Ali"
+class Person:
+    def __init__(self,name) -> None:
+        self.name=name
+        
