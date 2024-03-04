cbb8e1c784c5323271971df97bf85536b60e9e4a
QAmirkhan1177 <amirxonqarshiyev@gmail.com>
2024-03-04 17:44:45 +0500

    4 tayyor

--- create_some_objects.py
+++ create_some_objects.py
@@ -2,3 +2,8 @@ from create_one_attribute import Person
 
 #create an object named "p1" whose name is "Anvar"
 #create an object named "p2" whose name is "Shavkat"
+class Person:
+    def __init__(self,p1,p2) -> None:
+        self.p1=p1
+        self.p2=p2
+name=Person("Said","Anvar")
